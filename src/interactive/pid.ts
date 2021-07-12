f7dd8e97b5282b2bddb3a29da357c0f0e7a042a6
Fabio Spampinato <spampinabio@gmail.com>
2021-07-12 17:25:25 +0100

    PID: added support for fetching usage statistics

--- src/interactive/pid.ts
+++ src/interactive/pid.ts
@@ -2,6 +2,8 @@
 /* IMPORT */
 
 import pidtree from 'pidtree';
+import pidusage from 'pidusage';
+import {Usage} from '../types';
 
 /* MAIN */
 
@@ -30,6 +32,23 @@ const PID = {
       const pids = await PID.tree.get ( pid ) || treeFallback;
 
       pids.forEach ( PID.kill );
+
+    },
+
+    usage: async ( pid: number | undefined, treeFallback: (number | undefined)[] = [pid] ): Promise<Usage | undefined> => {
+
+      if ( !pid ) return;
+
+      const pids = await PID.tree.get ( pid ) || treeFallback;
+      const usages = ( await Promise.all ( pids.map ( PID.usage ) ) ).filter ( usage => usage ) as Usage[]; //TSC
+
+      if ( !usages.length ) return;
+
+      return usages.reduce ( ( acc, usage ) => ({
+        cpu: acc.cpu + usage.cpu,
+        memory: acc.memory + usage.memory,
+        timestamp: acc.timestamp
+      }), usages[0] );
 
     }
 
@@ -68,6 +87,12 @@ const PID = {
       return false;
 
     }
+
+  },
+
+  usage: ( pid: number ): Promise<Usage | undefined> => {
+
+    return pidusage ( pid );
 
   }
 
